8cc82102155b00193936da4b3a604cb7092e3fc1
ClementLalanne <clement.lalanne@ens.fr>
2018-12-18 20:08:06 +0100

    LEA added

--- src/point_to_anchor.py
+++ src/point_to_anchor.py
@@ -28,9 +28,48 @@ def matrix_association(data, anchors, s, matrix):
     return ret 
 
 def simplex_projection(z):
-    pass
+    v = np.sort(z)[::-1]
+    tmps = np.cumsum(v)
+    crit = v - (np.ones(z.shape[0]) / (np.arange(z.shape[0]) + 1)) * (tmps - np.ones(z.shape[0]))
+    rho = np.max(np.where(crit>0)) 
+    theta = 1/(rho+1) * (tmps[rho] - 1)
+    return ((z-np.full(z.shape[0], theta)) * ((z-np.full(z.shape[0], theta))>0))
+
 
 def LEA(data, anchors, s):
     K = anchors.shape[0]
     ret = np.zeros((data.shape[0], K), dtype = float)
-    return ret #Not implemented yet
+    c_ind = closest_indices(data, anchors, s)
+    for i in range(data.shape[0]):
+        U = anchors[c_ind[i], :].T 
+        def g(z):
+            return np.power(np.linalg.norm(data[i] - U.dot(z)),2)/2
+        def grad_g(z):
+            return U.T.dot(U.dot(z) - data[i])
+        def g_tilde(beta, v, z):
+            return g(v) + grad_g(v).T.dot(z-v) + beta*np.power(np.linalg.norm(z-v), 2)/2
+        z_old, z_new = np.ones(s)/s, np.ones(s)/s
+        delta_old = 0.
+        delta_new = 1.
+        beta_global = 1.
+        t=0
+        while True:
+            t +=1
+            alpha = (delta_old-1)/delta_new
+            v = z_new + alpha * (z_new-z_old)
+            j=0
+            while True:
+                beta = np.power(2, j) * beta_global
+                z = simplex_projection(v - 1/beta * grad_g(v))
+                if g(z) <= g_tilde(beta, v, z):
+                    beta_global = beta 
+                    z_old = z_new
+                    z_new = z
+                    break 
+            delta_old = delta_new
+            delta_new = (1+np.sqrt(1+4*delta_old**2))/2
+            if np.linalg.norm(z_old-z_new)<0.01:
+                break
+        for j in range(s):
+            ret[i, c_ind[j]] = z_new[j]
+    return ret 
